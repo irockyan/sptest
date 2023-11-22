e3b17428ef720dbe2887958bdf0ce0e40d6cb382
zhaoyan <zhaoyan@xiaoice.com>
2023-11-22 17:51:56 +0800

    init

---
+++ Person.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MyLibrary
+//
+//  Created by Zyan on 2023/11/22.
+//
+
+import Foundation
---
+++ Sources/MyLibrary/Person.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Zyan on 2023/11/22.
+//
+
+import Foundation
+
+public class Person {
+    public init() {
+    }
+    var name: String = ""
+    public func sayHello()  {
+        print("say hello")
+    }
+}
